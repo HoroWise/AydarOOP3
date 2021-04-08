0212c939d674f987021b3898354f6c604989a031
HoroWise <79516164+HoroWise@users.noreply.github.com>
2021-04-08 13:26:04 +0500

    class Human

--- AydarOOP3/AydarOOP3.cpp
+++ AydarOOP3/AydarOOP3.cpp
@@ -1,5 +1,40 @@
 ﻿#include <iostream>
+#include <string>
+#include <ctime>
 
+using namespace std;
+
+class Human {
+protected:
+	string name;
+	int age;
+	int weight;
+
+public:
+	Human() {
+		name = "RandomName";
+		age = 0;
+		weight = 0;
+	}
+
+	Human(string name, int age, int weight) {
+		this->name = name;
+		this->age = age;
+		this->weight = weight;
+	}
+
+	Human(const Human& h) {
+		name = h.name;
+		age = h.age;
+		weight = h.weight;
+	}
+
+	virtual void about_me() {
+		printf("Меня зовут %s,мне %d лет и я вешу %d кг.\n", name.c_str(), age, weight);
+	}
+
+	~Human() { }
+};
 
 int main()
 {
